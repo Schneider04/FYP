11aada33b95f127e71d3f8a5d2938ed52732a573
21016216 <108780210+Schneider04@users.noreply.github.com>
2023-08-02 12:48:33 +0800

    Add files via upload

---
+++ FYP app/Utils/TravelUtl.cs
@@ -0,0 +1,9 @@
+﻿namespace RP.SOI.DotNet.Utils;
+
+public static class TravelUtl
+{
+    public static string Abbreviate(this string story)
+    {
+        return story.Length < 30 ? story : story[..20] + "...";
+    }
+}
