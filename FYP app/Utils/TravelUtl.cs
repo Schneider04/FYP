fa86c4d3d6ae6195dda41c56103a0230215c2e60
PearlyLin <106571596+PearlyLin@users.noreply.github.com>
2023-08-05 13:05:35 +0800

    Delete FYP app directory

--- FYP app/Utils/TravelUtl.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RP.SOI.DotNet.Utils;
-
-public static class TravelUtl
-{
-    public static string Abbreviate(this string story)
-    {
-        return story.Length < 30 ? story : story[..20] + "...";
-    }
-}
