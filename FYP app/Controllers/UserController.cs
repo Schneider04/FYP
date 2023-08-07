2cd6c2f28d6a22573fb66afc0b5e5082d8662712
Pearly Lin <21022677@myrp.edu.sg>
2023-08-07 09:56:43 +0800

    create signup

---
+++ FYP app/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using FYPfinalWEBAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+using RP.SOI.DotNet.Utils;
+
+namespace FYPfinalWEBAPP.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult CreateUser(User usr)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                ViewData["Message"] = "Invalid input";
+                ViewData["MsgType"] = "warning";
+                return View("CreateUser");
+            }
+            else
+            {
+                string sql = @"INSERT INTO TravelUser(UserId, UserPw, FullName)
+                       VALUES('{0}', HASHBYTES('SHA1','{1}'), '{2}')"
+                ;
+                if (DBUtl.ExecSQL(sql, usr.UserId, usr.UserPw, usr.FullName) == 1)
+                {
+                    return View("~/Views/Account/Login.cshtml");
+                }
+                else
+                {
+                    ViewData["Message"] = DBUtl.DB_Message;
+                    ViewData["ExecSQL"] = DBUtl.DB_SQL;
+                    ViewData["MsgType"] = "danger";
+                    return View("CreateUser");
+                }
+            }
+        }
+    }
+}
--- FYP app/Models/User.cs
+++ FYP app/Models/User.cs
@@ -5,21 +5,15 @@ namespace FYPfinalWEBAPP.Models
 {
     public class User
     {
-
-        public string UserID { get; set; } = null!;
+        [Required(ErrorMessage = "Please enter User ID")]
+        public string UserId { get; set; } = null!;
 
         [Required(ErrorMessage = "Please enter Password")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Password must be 5-20 char")]
+        [DataType(DataType.Password)]
         public string UserPw { get; set; } = null!;
 
+        [Required(ErrorMessage = "Please enter Full Name")]
         public string FullName { get; set; } = null!;
-
-        [Required(ErrorMessage = "Please enter Email")]
-        public string UserEmail { get; set; } = null!;
-
-        public int HPno { get; set; }
-
-
-
-
     }
 }
